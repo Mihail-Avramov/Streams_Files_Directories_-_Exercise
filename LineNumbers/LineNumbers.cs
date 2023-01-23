288919a1bb88d8c463ea545cec10b4d2d8cad8ed
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-01-23 18:59:22 +0200

    LineNumber

--- LineNumbers/LineNumbers.cs
+++ LineNumbers/LineNumbers.cs
@@ -1,6 +1,9 @@
-﻿namespace LineNumbers
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LineNumbers
 {
-    using System;
     public class LineNumbers
     {
         static void Main()
@@ -13,7 +16,18 @@
 
         public static void ProcessLines(string inputFilePath, string outputFilePath)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            string[] lines = File.ReadAllLines(inputFilePath);
+            
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int lettersCount = lines[i].Count(char.IsLetter);
+                int punctuationCount = lines[i].Count(char.IsPunctuation);
+                sb.AppendLine($"Line {i + 1}: {lines[i]} ({lettersCount})({punctuationCount})");
+            }
+
+            File.WriteAllText(outputFilePath,sb.ToString());
+
         }
     }
 }
