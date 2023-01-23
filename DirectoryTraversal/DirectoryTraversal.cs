f1de54bc0591b1f80cdf2086295f0d9d1164da14
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-01-23 20:09:14 +0200

    Directory Traversal

--- DirectoryTraversal/DirectoryTraversal.cs
+++ DirectoryTraversal/DirectoryTraversal.cs
@@ -1,6 +1,11 @@
-﻿namespace DirectoryTraversal
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DirectoryTraversal
 {
-    using System;
     public class DirectoryTraversal
     {
         static void Main()
@@ -16,12 +21,44 @@
 
         public static string TraverseDirectory(string inputFolderPath)
         {
-            throw new NotImplementedException();
+            SortedDictionary<string, List<FileInfo>> extensionsFiles = new SortedDictionary<string, List<FileInfo>>();
+
+           string[] filesPaths = Directory.GetFiles(inputFolderPath);
+
+           foreach (var filePath in filesPaths)
+           {
+               FileInfo fileInfo = new FileInfo(filePath);
+
+               if (!extensionsFiles.ContainsKey(fileInfo.Extension))
+               {
+                    extensionsFiles.Add(fileInfo.Extension, new List<FileInfo>());
+               }
+
+               extensionsFiles[fileInfo.Extension].Add(fileInfo);
+           }
+
+           StringBuilder sb = new StringBuilder();
+
+           foreach (var extension in extensionsFiles
+                        .OrderByDescending(e => e.Value.Count))
+           {
+               sb.AppendLine(extension.Key);
+
+               foreach (var file in extension.Value.OrderBy(f => f.Length))
+               {
+                   sb.AppendLine($"--{file.Name} - {(double)file.Length / 1024:f3}kb");
+               }
+           }
+
+           Console.WriteLine(sb.ToString());
+
+            return sb.ToString();
         }
 
         public static void WriteReportToDesktop(string textContent, string reportFileName)
         {
-            throw new NotImplementedException();
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+            File.WriteAllText(filePath, textContent);
         }
     }
 }
