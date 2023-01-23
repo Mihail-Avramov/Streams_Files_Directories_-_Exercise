03a4aff1ca71a34f22765bd4d7143fe79f0391da
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-01-23 20:17:10 +0200

    2

--- DirectoryTraversal/DirectoryTraversal.cs
+++ DirectoryTraversal/DirectoryTraversal.cs
@@ -23,33 +23,33 @@ namespace DirectoryTraversal
         {
             SortedDictionary<string, List<FileInfo>> extensionsFiles = new SortedDictionary<string, List<FileInfo>>();
 
-           string[] filesPaths = Directory.GetFiles(inputFolderPath);
+            string[] filesPaths = Directory.GetFiles(inputFolderPath);
 
-           foreach (var filePath in filesPaths)
-           {
-               FileInfo fileInfo = new FileInfo(filePath);
+            foreach (var filePath in filesPaths)
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
 
-               if (!extensionsFiles.ContainsKey(fileInfo.Extension))
-               {
+                if (!extensionsFiles.ContainsKey(fileInfo.Extension))
+                {
                     extensionsFiles.Add(fileInfo.Extension, new List<FileInfo>());
-               }
+                }
 
-               extensionsFiles[fileInfo.Extension].Add(fileInfo);
-           }
+                extensionsFiles[fileInfo.Extension].Add(fileInfo);
+            }
 
-           StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
 
-           foreach (var extension in extensionsFiles
-                        .OrderByDescending(e => e.Value.Count))
-           {
-               sb.AppendLine(extension.Key);
+            foreach (var extension in extensionsFiles
+                         .OrderByDescending(e => e.Value.Count))
+            {
+                sb.AppendLine(extension.Key);
 
-               foreach (var file in extension.Value.OrderBy(f => f.Length))
-               {
-                   sb.AppendLine($"--{file.Name} - {(double)file.Length / 1024:f3}kb");
-               }
-           }
-           
+                foreach (var file in extension.Value.OrderBy(f => f.Length))
+                {
+                    sb.AppendLine($"--{file.Name} - {(double)file.Length / 1024:f3}kb");
+                }
+            }
+
             return sb.ToString();
         }
 
